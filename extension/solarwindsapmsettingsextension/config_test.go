6944329b02aff2f803e3a376491ce8f36981b235
Jerry Leung <tingfung.leung@solarwinds.com>
2024-01-19 14:59:38 -0800

    Updated config validation and test cases

--- extension/solarwindsapmsettingsextension/config_test.go
+++ extension/solarwindsapmsettingsextension/config_test.go
@@ -19,67 +19,117 @@ func TestValidate(t *testing.T) {
 			err:  errors.New("endpoint must not be empty"),
 		},
 		{
-			name: "empty key",
+			name: "valid configuration",
 			cfg: &Config{
-				Endpoint: "host:12345",
-			},
-			err: errors.New("key must not be empty"),
-		},
-		{
-			name: "invalid endpoint",
-			cfg: &Config{
-				Endpoint: "invalid",
+				Endpoint: "apm.collector.na-02.cloud.solarwinds.com:443",
 				Key:      "token:name",
-			},
-			err: errors.New("endpoint should be in \"<host>:<port>\" format"),
-		},
-		{
-			name: "invalid endpoint 2",
-			cfg: &Config{
-				Endpoint: "host:12345:12345",
-				Key:      "invalid",
-			},
-			err: errors.New("endpoint should be in \"<host>:<port>\" format"),
-		},
-		{
-			name: "invalid endpoint format but port is not an integer",
-			cfg: &Config{
-				Endpoint: "host:abc",
-				Key:      "token:name",
-			},
-			err: errors.New("the <port> portion of endpoint has to be an integer"),
-		},
-		{
-			name: "invalid key",
-			cfg: &Config{
-				Endpoint: "host:12345",
-				Key:      "invalid",
-			},
-			err: errors.New("key should be in \"<token>:<service_name>\" format"),
-		},
-		{
-			name: "invalid key 2",
-			cfg: &Config{
-				Endpoint: "host:12345",
-				Key:      "invalid:name:name",
-			},
-			err: errors.New("key should be in \"<token>:<service_name>\" format"),
-		},
-		{
-			name: "valid",
-			cfg: &Config{
-				Endpoint: "host:12345",
-				Key:      "token:name",
-				Interval: "1s",
+				Interval: "10s",
 			},
 			err: nil,
 		},
 		{
+			name: "endpoint without :",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com",
+			},
+			err: errors.New("endpoint should be in \"<host>:<port>\" format"),
+		},
+		{
+			name: "endpoint with some :",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:a:b",
+			},
+			err: errors.New("endpoint should be in \"<host>:<port>\" format"),
+		},
+		{
+			name: "endpoint with invalid point",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:port",
+			},
+			err: errors.New("the <port> portion of endpoint has to be an integer"),
+		},
+		{
+			name: "bad endpoint",
+			cfg: &Config{
+				Endpoint: "apm.collector..cloud.solarwinds.com:443",
+			},
+			err: errors.New("endpoint \"<host>\" part should be in \"apm.collector.[a-z]{2,3}-[0-9]{2}.[a-z\\-]*.solarwinds.com\" regex format, see https://documentation.solarwinds.com/en/success_center/observability/content/system_requirements/endpoints.htm for detail"),
+		},
+		{
+			name: "empty endpoint with port",
+			cfg: &Config{
+				Endpoint: ":433",
+			},
+			err: errors.New("endpoint should be in \"<host>:<port>\" format and \"<host>\" must not be empty"),
+		},
+		{
+			name: "empty endpoint without port",
+			cfg: &Config{
+				Endpoint: ":",
+			},
+			err: errors.New("endpoint should be in \"<host>:<port>\" format and \"<host>\" must not be empty"),
+		},
+		{
+			name: "endpoint without port",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:",
+			},
+			err: errors.New("endpoint should be in \"<host>:<port>\" format and \"<port>\" must not be empty"),
+		},
+		{
+			name: "valid endpoint but empty key",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
+			},
+			err: errors.New("key must not be empty"),
+		},
+		{
+			name: "key is :",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
+				Key:      ":",
+			},
+			err: errors.New("key should be in \"<token>:<service_name>\" format and \"<token>\" must not be empty"),
+		},
+		{
+			name: "key is ::",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
+				Key:      "::",
+			},
+			err: errors.New("key should be in \"<token>:<service_name>\" format and \"<token>\" must not be empty"),
+		},
+		{
+			name: "key is :name",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
+				Key:      ":name",
+			},
+			err: errors.New("key should be in \"<token>:<service_name>\" format and \"<token>\" must not be empty"),
+		},
+		{
+			name: "key is token:",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
+				Key:      "token:",
+			},
+			err: errors.New("key should be in \"<token>:<service_name>\" format and \"<service_name>\" must not be empty"),
+		},
+		{
 			name: "empty_interval",
 			cfg: &Config{
-				Endpoint: "host:12345",
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
 				Key:      "token:name",
 				Interval: "",
+			},
+			err: errors.New("interval must not be empty"),
+		},
+		{
+			name: "interval is not a duration string",
+			cfg: &Config{
+				Endpoint: "apm.collector.na-01.cloud.solarwinds.com:443",
+				Key:      "token:name",
+				Interval: "something",
 			},
 			err: errors.New("interval has to be a duration string. Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\""),
 		},
